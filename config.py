8a72b31450f87877393d144468dc371d07f50cf8
yanbhliu <yanbhliu15@gmail.com>
2022-07-27 09:56:53 -0400

    evaluate KITTI

--- config.py
+++ config.py
@@ -1,111 +1,95 @@
-####################
-# GENERAL SETTINGS #
-####################
-''' DATA LOADER '''
-# number of points for the input clouds
-NUM_POINTS = 4096
-# data path
-DATASET_FOLDER = '/home/cel/data/benchmark_datasets'
+# GLOBAL
+EXP_NAME = 'atten_ds2048_epn_32_64_netvlad_1024_trainall'
+MODEL = 'atten_epn_netvlad_select'
+# testing: 'epn_netvlad', 'epn_atten_netvlad', 'atten_epn_netvlad', 'atten_epn_netvlad_select'
+# archived: 'pointnetepn_netvlad', 'pointnetvlad_epnnetvlad' 
 
-#####################
-# TRAINING SETTINGS #
-#####################
-''' GLOBAL '''
-# specify the experient name, training process will be saved in the folder with the same name
-EXP_NAME = 'atten_epn_netvlad_test' 
-# choose the model to be trained from 'epn_netvlad' or 'atten_epn_netvlad'
-MODEL = 'atten_epn_netvlad'
-
-''' TRAINING PICKLE FILES '''
-# use the picke files in the following two lines for whole benchmark training
-TRAIN_FILE = '/home/cel/data/benchmark_datasets/training_queries_baseline.pickle'
-TEST_FILE = '/home/cel/data/benchmark_datasets/test_queries_baseline.pickle'
-# use the picke files in the following two lines for a quick training with only 3 sequences in the benchmark
-# TRAIN_FILE = '/home/cel/data/benchmark_datasets/training_queries_baseline_seq567.pickle'
-# TEST_FILE = '/home/cel/data/benchmark_datasets/test_queries_baseline_seq567.pickle'
-
-''' NETWORK DIMENSIONS '''
-# output dimension of the global descriptor
-GLOBAL_DESCRIPTOR_DIM = 256
-# output dimension of the local feature
-LOCAL_FEATURE_DIM = 1024
-# number of points after attentive downsampling, not used if training with model 'epn_netvlad'
-NUM_SELECTED_POINTS = 2048
-
-''' DATA LOADER '''
-# batch number setting, how many positive and negatives per query during training
+# DATA LOADER
+# DATASET = 'Oxford' # 'KITTI', 'ModelNet40'
+DATASET = 'KITTI' # 'KITTI'
+DATASET_FOLDER = '/home/yanbhliu/dataset/'
+NUM_POINTS = 4096 # 4096
 BATCH_NUM_QUERIES = 1
-TRAIN_POSITIVES_PER_QUERY = 2
-TRAIN_NEGATIVES_PER_QUERY = 16
-# Is this training resume from previous training?
-RESUME = False
+TRAIN_POSITIVES_PER_QUERY = 1
+TRAIN_NEGATIVES_PER_QUERY = 1
+NORMAL = False
 
-''' TRAINNING PARAMETERS '''
+# TRAIN
 MAX_EPOCH = 30
-BASE_LEARNING_RATE = 0.00005
+BASE_LEARNING_RATE = 0.00005 #0.000005
 MOMENTUM = 0.9
 OPTIMIZER = 'adam'
 DECAY_STEP = 200000
 DECAY_RATE = 0.7
 
-''' PARAMETERS FOR LOSS FUNCTION '''
-LOSS_FUNCTION = 'quadruplet'
+# LOSS
+LOSS_FUNCTION = 'quadruplet' # 'quadruplet'
 MARGIN_1 = 0.5
 MARGIN_2 = 0.2
 LOSS_LAZY = True
 LOSS_IGNORE_ZERO_BATCH = False
 TRIPLET_USE_BEST_POSITIVES = False
 
-''' SAVE PATH '''
+# NETWORK
+# FEATURE_OUTPUT_DIM = 256 #128 # global descriptor
+GLOBAL_DESCRIPTOR_DIM = 256
+LOCAL_FEATURE_DIM = 1024 #1024
+NUM_SELECTED_POINTS = 2048 #4096 #300 #1024 #256
+
+# SAVE PATH
 LOG_DIR = 'log/'
 MODEL_FILENAME = "model.ckpt"
 
-''' OTHERS '''
-# setting GPU usage with CUDA_VISIBLE_DEVICES
+
+# OTHERS
 GPU = '0'
 
+BN_INIT_DECAY = 0.5
+BN_DECAY_DECAY_RATE = 0.5
+BN_DECAY_CLIP = 0.99
 
-#######################
-# EVALUATION SETTINGS #
-#######################
-''' GLOBAL '''
-# choose the model to evaluate from 'epn_netvlad' or 'atten_epn_netvlad'
+RESUME = True
+
+TRAIN_FILE = '/home/cel/data/benchmark_datasets/training_queries_baseline.pickle'
+TEST_FILE = '/home/cel/data/benchmark_datasets/test_queries_baseline.pickle'
+# TRAIN_FILE = '/home/cel/data/benchmark_datasets/training_queries_baseline_seq567.pickle'
+# TEST_FILE = '/home/cel/data/benchmark_datasets/test_queries_baseline_seq567.pickle'
+
+# EVAL_MODEL = 'vn_dgcnn_netvlad'
+# RESULTS_FOLDER = 'results/pr_evaluation_vn_dgcnn_netvlad'
+# OUTPUT_FILE = RESULTS_FOLDER+'/results.txt'
+# RESUME_FILENAME = 'pretrained_models/dgcnn_related/vn_dgcnn_netvlad.ckpt'
+
+
+# EVALUATION PLACE RECOGNITION KITTI
 EVAL_MODEL = 'atten_epn_netvlad'
-# the pretrained weights that you want to load into the model
-# RESUME_FILENAME = 'pretrained_models/test.ckpt'
-RESUME_FILENAME = LOG_DIR+EXP_NAME+'/'+MODEL_FILENAME
-# save paths for the evaluation results
-RESULTS_FOLDER = 'results/pr_evaluation_atten_epn_netvlad_test'
+RESULTS_FOLDER = 'results/kitti_evaluation_EPN_pointnetvlad_00'
 OUTPUT_FILE = RESULTS_FOLDER+'/results.txt'
+RESUME_FILENAME = '/home/yanbhliu/pretrained_model/atten_epn_netvlad_na20_trainall.ckpt'
 
-''' DATA LOADER '''
-# batch number setting, how many positive and negatives per query during evaluation
+
 EVAL_BATCH_SIZE = 1
 EVAL_POSITIVES_PER_QUERY = 2
 EVAL_NEGATIVES_PER_QUERY = 2
 
-''' EVALUATION PICKLE FILES '''
-# use the picke files in the following two lines for oxford benchmark evaluation
-EVAL_DATABASE_FILE = '/home/cel/data/benchmark_datasets/oxford_evaluation_database.pickle'
-EVAL_QUERY_FILE = '/home/cel/data/benchmark_datasets/oxford_evaluation_query.pickle'
-# use the picke files in the following two lines for U.S. benchmark evaluation
-# EVAL_DATABASE_FILE = '/home/cel/data/benchmark_datasets/university_evaluation_database.pickle'
-# EVAL_QUERY_FILE = '/home/cel/data/benchmark_datasets/university_evaluation_query.pickle'
-# use the picke files in the following two lines for B.D. benchmark evaluation
-# EVAL_DATABASE_FILE = '/home/cel/data/benchmark_datasets/business_evaluation_database.pickle'
-# EVAL_QUERY_FILE = '/home/cel/data/benchmark_datasets/business_evaluation_query.pickle'
-# use the picke files in the following two lines for R.A. benchmark evaluation
-# EVAL_DATABASE_FILE = '/home/cel/data/benchmark_datasets/residential_evaluation_database.pickle'
-# EVAL_QUERY_FILE = '/home/cel/data/benchmark_datasets/residential_evaluation_query.pickle'
 
-'''BASELINES TO PLOT (OPTIONAL)'''
-POINTNETVLAD_RESULT_FOLDER = 'results/pr_evaluation_pointnetvlad'
-SCANCONTEXT_RESULT_FOLDER = 'results/pr_evaluation_scan_context_oxford_evalall'
-M2DP_RESULT_FOLDER = 'results/pr_evaluation_m2dp_evalall'
-BASELINE_RESULT_FOLDER = 'results/pr_evaluation_atten_epn_netvlad_select_seq567_32_64_1024_ds2048_evalall'
+EVAL_DATABASE_FILE = "/home/yanbhliu/KITTI_evaluate/kitti_00_database_evaluate_new.pickle"
+EVAL_QUERY_FILE = "/home/yanbhliu/KITTI_evaluate/kitti_00_queries_evaluate_new.pickle"
+# EVAL_DATABASE_FILE = "/home/yanbhliu/KITTI_evaluate/KITTI_00_database_samp10.pickle"
+# EVAL_QUERY_FILE = "/home/yanbhliu/KITTI_evaluate/KITTI_00_query_samp10.pickle"
+
+# EVAL_DATABASE_FILE = "/home/yanbhliu/KITTI_evaluate/kitti_00_database_evaluate.pickle"
+# EVAL_QUERY_FILE = "/home/yanbhliu/KITTI_evaluate/kitti_00_queries_evaluate.pickle"
+# EVAL_DATABASE_FILE = "/home/yanbhliu/KITTI_evaluate/loop_08_GT_4m.pickle"
+# EVAL_QUERY_FILE  = "/home/yanbhliu/KITTI_evaluate/loop_08_GT_4m.pickle"
 
 
-'''cofig to string'''
+# VNN
+N_KNN = 20
+POOLING = 'mean'
+CLUSTER_SIZE = 64
+
+
 def cfg_str():
     out_string = ""
     for name in globals():
